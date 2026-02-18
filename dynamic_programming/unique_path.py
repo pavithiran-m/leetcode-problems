6be05fe3cd995b92b16e931afd7b62bdba519f41
pavithian <pavithiran1008@gmail.com>
2026-02-18 10:14:25 -0700

    unique path

---
+++ dynamic_programming/unique_path.py
@@ -0,0 +1,15 @@
+def uniquePaths(m,n):
+    dp=[1]*n
+
+    for i in range(1,m):
+        for j in range(1,n):
+            dp[j]=dp[j]+dp[j-1]
+    return dp[n-1]
+
+m=3
+n=7     
+print(uniquePaths(m,n))
+
+    
+
+        
